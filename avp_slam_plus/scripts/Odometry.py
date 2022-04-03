9785a578c69294810c3503c45661016f7f9a2ba2
ChunHsuehLee <chunhlee@umich.edu>
2022-04-03 10:52:44 -0400

    done odometry, need tunning

--- avp_slam_plus/scripts/Odometry.py
+++ avp_slam_plus/scripts/Odometry.py
@@ -4,17 +4,18 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 import numpy as np
+from math import sin, cos
 
 gazebo_c1 = 0.05
 gazebo_c2 = 0.05
 gazebo_c3 = 0.05
 gazebo_c4 = 0.05
 
-odom_c1 = 0.1
-odom_c2 = 0.1
-odom_c3 = 0.1
-odom_c4 = 0.1
-dt = 1.0/100
+odom_c1 = 0.01
+odom_c2 = 0.01
+odom_c3 = 0.01
+odom_c4 = 0.01
+dt = 1.0/10
 id = 0
 pre_id = -1
 
@@ -52,10 +53,25 @@ def odom_callback(data):
     global pre_id
     # odom_vel_cmd = Twist()
     delta_t = dt
-    o_R = np.array([[(gazebo_c1* abs(v)+gazebo_c2*abs(w))**2, 0], [0, (gazebo_c3* abs(v)+gazebo_c4*abs(w))**2]])
+    theta = previous_pose[2]
+
+
+
+    
+    o_R = np.array([[(odom_c1* abs(v)+odom_c2*abs(w))**2, 0], [0, (odom_c3* abs(v)+odom_c4*abs(w))**2]]) + 1e-10
     o_rand = np.random.multivariate_normal([v, w], o_R)
     v_odom = o_rand[0]
     w_odom = o_rand[1]
+
+    Vt = np.array([[ (-sin(theta)+sin(theta+w_odom*dt))/w_odom,  v_odom*(sin(theta)-sin(theta+w_odom*dt))/(w_odom**2) + (v_odom*cos(theta+w_odom*dt)*dt)/w_odom ],
+                [ (cos(theta)-cos(theta+w_odom*dt))/w_odom,  -v_odom*(cos(theta)-cos(theta+w_odom*dt))/(w_odom**2) + (v_odom*sin(theta+w_odom*dt)*dt)/w_odom ],
+                [0, dt]])
+
+    # print("R ", o_R)
+    # print("V ", Vt)
+    cov = np.matmul(np.matmul(Vt, o_R), Vt.T)
+    # print(cov)
+    # print((-sin(theta)+sin(theta+w_odom*dt))/w_odom)
     # print("odom: ", v_odom, w_odom)
 
 #     # print("v_actual", v_actual)
@@ -67,14 +83,15 @@ def odom_callback(data):
     new_pose = previous_pose + delta_pose
     # print(new_pose)
     vertex_output = "VERTEX_SE2" + " " +  str(id) + " " + str(new_pose[0]) + " " + str(new_pose[1]) + " " + str(new_pose[2])
-    edge_output = "EDGE_SE2" + " " + str(pre_id) + " " + str(id) + " " + str(delta_x) + " " + str(delta_y) + " " + str(delta_theta) + " " + str(o_R[0, 0]) + " " + str(o_R[1, 1])
+    edge_output = "EDGE_SE2" + " " + str(pre_id) + " " + str(id) + " " + str(delta_x) + " " + str(delta_y) + " " + str(delta_theta) \
+        + " " + str(cov[0, 0]) + " " + str(cov[0, 1]) + " " + str(cov[0, 2]) + " " + str(cov[1, 1]) + " " + str(cov[1, 2]) + " " + str(cov[2, 2])
     print(vertex_output)
     print(edge_output)
     previous_pose = new_pose
     id += 1
     pre_id += 1 
-    edge_pub.publish(edge_output)    
-    vertex_pub.publish(vertex_output)    
+    # edge_pub.publish(edge_output)    
+    # vertex_pub.publish(vertex_output)    
 
 #     return new_pose
 
